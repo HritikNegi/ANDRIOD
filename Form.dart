1315c56935941453c2841e086815e63e6f945b44
HritikNegi <91362469+HritikNegi@users.noreply.github.com>
2024-12-21 21:00:25 +0530

    Update Form.dart

--- Form.dart
+++ Form.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'SELF DECLARATION FORM',
-      theme: ThemeData(),
+      theme: ThemeData( primarySwatch: Colors.blueGrey),
       home:  MyForm(),
     );
   }
